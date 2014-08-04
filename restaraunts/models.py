2695d1a81f16f0a81acbedabad972fe50b272f9f
Aishwarya Seeram <aishwarya.seeram@cerner.com>
2014-08-04 08:33:45 -0500

    Committing my files

---
+++ restaraunts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Topping(models.Model):
+    name = models.CharField(max_length=30)
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+    toppings = models.ManyToManyField(Topping)
+
+class Restaurant(models.Model):
+    pizzas = models.ManyToManyField(Pizza, related_name='restaurants')
+    best_pizza = models.ForeignKey(Pizza, related_name='championed_by')
